79760b61ace550fccf651b11cd2032203dbc0bb6
syedamir5560 <133253595+syedamir5560@users.noreply.github.com>
2023-05-21 13:04:12 +0530

    Add files via upload

---
+++ springboot-registration-login/target/src/main/java/com/yakub/WebApplication.java
@@ -0,0 +1,13 @@
+package com.yakub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
